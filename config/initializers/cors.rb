89c5a6f5e33932aa956b8a47e4dbf9f5d75181e4
aidinca <aidinca@streamwide.com>
2026-02-04 13:15:48 +0200

    fixed login issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:*", "http://127.0.0.1:*"
+    origins "http://localhost:9000", "http://127.0.0.1:9000", "http://localhost:3000", "http://127.0.0.1:3000"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options],
-      expose: ["access-token", "client", "uid"]
+      methods: [:get, :post, :patch, :put, :delete, :options, :head],
+      credentials: true,
+      expose: ["access-token", "client", "uid", "expiry", "token-type", "Authorization"]
   end
 end
--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -63,5 +63,8 @@ config.default_confirm_success_url = "http://localhost:9000/auth/callback"
   # By default DeviseTokenAuth will not send confirmation email, even when including
   # devise confirmable module. If you want to use devise confirmable module and
   # send email, set it to true. (This is a setting for compatibility)
-  # config.send_confirmation_email = true
+  config.send_confirmation_email = false
+  
+  # Skip confirmation for development
+  config.default_confirm_success_url = "http://localhost:9000/auth/callback"
 end
